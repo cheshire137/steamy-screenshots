4b7cd804295c93b9a7d60fff71c46ce9f821c717
Sarah Vessels <cheshire137@gmail.com>
2015-04-28 00:34:22 -0400

    Read RSS service URI from env var

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'json'
 
 get '/' do
   redirect '/index.html'
@@ -9,4 +10,14 @@ get '/image' do
   file = open(image_url)
   content_type file.content_type
   file
+end
+
+get '/config.json' do
+  content_type 'application/json'
+  path = 'public/config.json'
+  if File.exist?(path)
+    File.read(path)
+  else
+    {rssServiceUrl: ENV['RSS_SERVICE_URL']}.to_json
+  end
 end
