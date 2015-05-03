c511f41499c9fdfb65e0cc72eb30f35ea7c02c04
Sarah Vessels <cheshire137@gmail.com>
2015-05-03 12:22:10 -0400

    Remove 'more user screenshots' link

--- public/js/app.js
+++ public/js/app.js
@@ -393,8 +393,7 @@ function listImagesFromRSS(page) {
 function resetUser() {
   $('.no-screenshots').hide();
   $('#steam-friends-dropdown').empty();
-  $('.screenshot-wrapper, .error-message, ' +
-    '.steam-user-screenshots-link').fadeOut('fast');
+  $('.screenshot-wrapper, .error-message').fadeOut('fast');
   $('.pagination').empty().fadeOut('fast');
   $('body, a, .page-title, .top-nav, nav .input-field label.active i, ' +
     'input, .metadata .swatch, #steam-friends-dropdown, .page-footer').
@@ -404,7 +403,7 @@ function resetUser() {
     '.top-nav .steam-app-name').fadeOut('fast').text('');
   $('.steam-app-wrapper').fadeOut('fast');
   $('.steam-app-link').attr('href', '');
-  $('.steam-app-name, .steam-user-screenshots-link .steam-user-name').text('');
+  $('.steam-app-name').text('');
   $('.steam-user-profile-link').attr('href', '').fadeOut('fast');
   $('#search-container').fadeOut('fast');
 }
@@ -451,13 +450,6 @@ function listImagesFromAppScreenshots(page) {
         $('.steam-user-profile-link').attr('href', screenshot.userUrl).
                                       fadeIn('fast');
       }
-      // TODO: check if user has public screenshots before linking
-      // if (screenshot.userName && screenshot.userName !== '') {
-      //   $('.steam-user-screenshots-link .steam-user-name').
-      //       text(screenshot.userName);
-      //   var path = '#steam/user/' + screenshot.userName;
-      //   $('.steam-user-screenshots-link').fadeIn('fast').attr('href', path);
-      // }
     }
     var url = screenshot.fullSizeUrl || screenshot.mediumUrl;
     listImage(screenshot.detailsUrl, url, screenshot.title, i + 1, page);
